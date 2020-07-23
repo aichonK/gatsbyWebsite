ee32913852eb58ab147b558fbe6169025d54a914
aicha <aichabenchaoucha@gmail.com>
2020-07-23 11:45:50 +0200

    Add a progressive navbar

---
+++ src/components/ArticlesComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlesComponent = () => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
+
+export default ArticlesComponent
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
-import { AiFillPlusCircle } from "react-icons/ai";
+import React, { useState } from "react"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { IoIosCloseCircle } from "react-icons/io"
 const Pages = [{
   name: "Home",
   url: "/"
@@ -19,52 +20,67 @@ const Pages = [{
   url: "/contact"
 }]
 
-const Header = ({ siteTitle }) => (
-  <header
-  className="bg-gray-800 mb-6"
-    
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <h2 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+const Header = ({ siteTitle }) => {
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false)
+  return (
+    <header className="bg-gray-800 sm:flex sm:justify-between sm:px-4 sm:py-2">
+      <div className="flex item-center justify-between px-4 py-2 sm:p-0">
+        <h2 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            className="text-gray-500 font-bold"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h2>
+        <button
+          className=" block focus:outline-none  focus:text-gray-100 hover:text-gray-100 text-gray-500 sm:hidden"
+          onClick={() => setMobileMenuIsShown(!mobileMenuIsShown)}
         >
-          {siteTitle}
-        </Link>
-      </h2>
-      <div>
-        {" "}
+          <GiHamburgerMenu
+            className={`h-6 w-6 ${mobileMenuIsShown ? "hidden" : ""}`}
+          />
+          <IoIosCloseCircle
+            className={`h-6 w-6 ${mobileMenuIsShown ? "" : "hidden"}`}
+          />
+        </button>
+      </div>
+      <div
+        class={`px-2 pb-4 sm:flex ${mobileMenuIsShown ? "block" : "hidden"}`}
+      >
         {Pages.map(({ name, url }) => (
           <Link
+            className="text-white block  font-semibold rounded hover:bg-gray-700 
+              pl-2 py-1
+               mt-1 sm:mx-2 sm:py-0 sm:my-0"
             to={url}
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-              marginRight: "10px",
-            }}
           >
             {name}
           </Link>
         ))}
-        <button>
-          <AiFillPlusCircle className="h-6 w-6"/>
-        </button>
       </div>
-    </div>
-  </header>
-)
+      
+      {/* <div className="flex">
+      {" "}
+      {Pages.map(({ name, url }) => (
+        <Link
+          className="text-gray-500"
+          to={url}
+          style={{
+            textDecoration: `none`,
+            marginRight: "10px",
+          }}
+        >
+          {name}
+        </Link>
+      ))}
+    </div> */}
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
